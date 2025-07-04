644eff9baaa2860224271c7896ad8de5d3eac642
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-04 14:33:25 +0300

    added real partitioning on bookings table using PARTITION BY

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -20,3 +20,28 @@ SELECT * FROM bookings_2022 WHERE start_date = '2022-08-01';
 
 EXPLAIN QUERY PLAN
 SELECT * FROM bookings_2023 WHERE start_date = '2023-03-15';
+
+
+
+
+-- Drop the table if it already exists
+DROP TABLE IF EXISTS bookings;
+
+-- Create a partitioned bookings table by RANGE of year(start_date)
+CREATE TABLE bookings (
+    id INT NOT NULL,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    payment_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    PRIMARY KEY (id, start_date)
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+    PARTITION p2022 VALUES LESS THAN (2023),
+    PARTITION p2023 VALUES LESS THAN (2024),
+    PARTITION pmax VALUES LESS THAN MAXVALUE
+);
+
+-- Optional: Test performance using EXPLAIN
+EXPLAIN
+SELECT * FROM bookings WHERE start_date BETWEEN '2022-01-01' AND '2022-01-31';
