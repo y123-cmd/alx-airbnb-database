165909e851b5c19af565229383069ba169fc0299
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-04 14:27:11 +0300

    added where clause in the perfomance.sql file

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -20,3 +20,5 @@ FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.payment_id = pay.id;
+WHERE u.name = 'Alice' AND pay.amount > 100;
+
