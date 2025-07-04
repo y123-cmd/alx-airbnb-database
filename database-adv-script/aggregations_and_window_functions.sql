b0ac472f6744545be2f30aa599c16d0beadebae4
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-04 13:16:17 +0300

    updated the sql by adding the row_number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,7 +8,7 @@ GROUP BY users.name;
 SELECT 
   properties.name AS property_name,
   COUNT(bookings.id) AS total_bookings,
-  RANK() OVER (ORDER BY COUNT(bookings.id) DESC) AS rank
+  ROW_NUMBER() OVER (ORDER BY COUNT(bookings.id) DESC) AS row_number
 FROM properties
 LEFT JOIN bookings ON properties.id = bookings.property_id
 GROUP BY properties.name;
