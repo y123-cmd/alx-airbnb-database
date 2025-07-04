59f6ee1c2c27f1147551e9157c08dc11ae8a6bfb
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-04 13:37:48 +0300

    added explain analyze in the database_index.sql file

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -9,3 +9,19 @@ CREATE INDEX IF NOT EXISTS idx_users_id ON users(id);
 
 -- Index on id in properties table
 CREATE INDEX IF NOT EXISTS idx_properties_id ON properties(id);
+
+
+
+-- Measure performance before indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 1;
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX IF NOT EXISTS idx_bookings_property_id ON bookings(property_id);
+CREATE INDEX IF NOT EXISTS idx_users_id ON users(id);
+CREATE INDEX IF NOT EXISTS idx_properties_id ON properties(id);
+
+-- Measure performance after indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 1;
