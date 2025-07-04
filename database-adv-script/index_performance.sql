70c48e9db60b828e7b76e023758f65480b196998
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-04 13:33:37 +0300

    removed explain analyze from the sql to omit the errors

--- database-adv-script/index_performance.sql
+++ database-adv-script/index_performance.sql
@@ -12,16 +12,4 @@ CREATE INDEX idx_properties_id ON properties(id);
 
 
 
--- Before indexing
-EXPLAIN QUERY PLAN
-SELECT * FROM bookings WHERE user_id = 1;
 
--- Indexes
-CREATE INDEX IF NOT EXISTS idx_bookings_user_id ON bookings(user_id);
-CREATE INDEX IF NOT EXISTS idx_bookings_property_id ON bookings(property_id);
-CREATE INDEX IF NOT EXISTS idx_users_id ON users(id);
-CREATE INDEX IF NOT EXISTS idx_properties_id ON properties(id);
-
--- After indexing
-EXPLAIN QUERY PLAN
-SELECT * FROM bookings WHERE user_id = 1;
